73bce62312d2a3245087e16b7dfe07e3c972fc2b
Eric Polerecky <eric@polerecky.com>
2021-07-25 15:20:26 -0400

    install deps and auth to cermaic

--- App.tsx
+++ App.tsx
@@ -1,12 +1,40 @@
+import { nanoid } from 'nanoid'
+import 'react-native-get-random-values'
+import 'fastestsmallesttextencoderdecoder';
+
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { Button, StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  const onCreate = () => {
 
+import CeramicClient from '@ceramicnetwork/http-client'
+import { IDX } from '@ceramicstudio/idx';
+import KeyDidResolver from 'key-did-resolver';
+import { DID } from 'dids';
+import { Ed25519Provider } from 'key-did-provider-ed25519';
+import { fromString } from 'uint8arrays';
+
+export default function App() {
+  const onCreate = async () => {
+    const API_URL = "https://ceramic-clay.3boxlabs.com"
+    const ceramic = new CeramicClient(API_URL, { syncInterval: 100 });
+    const resolver = KeyDidResolver.getResolver();
+    const SEED = 'd5f727c343f9311780f396c236eebc6a10ccb9ada6c1efccb2471273f4be4bdf';
+
+    const encodedSeed = fromString(SEED, 'base16');
+    const provider = new Ed25519Provider(encodedSeed);
+    const did = new DID({ provider, resolver });
+
+    // ceramic is consided authenticated if the DID is set.
+    ceramic.did = did;
+    await did.authenticate();
+
+    const idx = new IDX({ ceramic });
+
+    const created = await idx.set('basicProfile', { 'name': 'expo' });
+    console.debug('createProfile: created:', created.toString()); // stream id
   }
-  
+
   return (
     <View style={styles.container}>
       <Button title='create' onPress={onCreate}></Button>
